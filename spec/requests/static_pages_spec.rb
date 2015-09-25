b4cf29e213d3a3d5a5db66749bdba57bad97a450
Kota Ishimoto <ishikota086@gmail.com>
2015-09-25 18:58:20 +0900

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,27 +3,26 @@ require 'spec_helper'
 describe "Static pages" do
 
   subject { page }
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
-    before { visit '/static_pages/home' }
-    it { should  have_title("#{base_title}") }
+    before { visit root_path }
+    it { should  have_title(full_title('')) }
     it { should_not have_title(" | Home") }
   end
 
   describe "Help page" do
-    before { visit '/static_pages/help' }
-    it { should have_title("#{base_title} | Help") }
+    before { visit help_path }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
-    before { visit '/static_pages/about' }
-    it { should have_title("#{base_title} | About Us") }
+    before { visit about_path }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
-    before { visit '/static_pages/contact' }
-    it { should have_title("#{base_title} | Contact") }
+    before { visit contact_path }
+    it { should have_title(full_title('Contact')) }
   end
 
 end
