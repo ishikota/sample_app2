fc12374a47ffee967d34c1f38a36cb5552d1817e
Kota Ishimoto <ishikota086@gmail.com>
2015-09-26 20:09:01 +0900

    Finish chapter 8 before cucumber

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
   it { should be_valid }
 
   describe "when name is not present" do
@@ -97,6 +98,11 @@ describe User do
       @user.save
       expect(@user.email).to eq mixed_case_ad.downcase
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
 
